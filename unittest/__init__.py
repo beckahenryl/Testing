3e8a5f2766446517b8ce04b6dcb90f865feb7f97
Becka Henry <36617288+beckahenryl@users.noreply.github.com>
2019-01-25 08:56:51 -0800

    Add files via upload

---
+++ unittest/__init__.py
@@ -0,0 +1,8 @@
+def sum(arg):
+	total = 0
+	for val in arg:
+		total+= val
+	return total
+#create variable total that iterates over all values
+#and adds them to total. It then returns the result once
+#the iterable has been exhausted		
