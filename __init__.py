92ab9d741b85c42122ea4c44f320581f295ce9e5
Becka Henry <36617288+beckahenryl@users.noreply.github.com>
2019-01-25 08:57:32 -0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-def sum(arg):
-	total = 0
-	for val in arg:
-		total+= val
-	return total
-#create variable total that iterates over all values
-#and adds them to total. It then returns the result once
-#the iterable has been exhausted		
